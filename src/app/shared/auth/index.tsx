c86e9f92a7292310dca441bb24ca62b3c1558660
Karol Binkowski <starystachu9@gmail.com>
2023-07-22 17:16:15 +0200

    fix: fix type export

--- src/app/shared/auth/index.tsx
+++ src/app/shared/auth/index.tsx
@@ -1,4 +1,5 @@
 export * from './adapters';
-export { AuthProvider, AuthProviderProps, getAuth } from './context/auth-provider';
+export { AuthProvider, getAuth } from './context/auth-provider';
+export type { AuthProviderProps } from './context/auth-provider';
 export * from './hooks';
 export * from './types';
